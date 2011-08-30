94cc763a5e46a478310e28f97eb6f5188eb1d31d
Michael Green <michael.green@infospace.com>
2011-08-30 12:20:04 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scribbler::Application.config.secret_token = '07221b1bf060d4c303a658913ac6a5ae1c4394397beeddb403c650b60477378d151c8106c1c2af8307998e2527ee0011266b9a9e00792283d748efe768f0045f'
